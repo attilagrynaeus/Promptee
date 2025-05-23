2371ca48d2dc3ca24f4971c15cd0421adf98deb6
Attila Grynaeus <212490695+attilagrynaeus@users.noreply.github.com>
2025-05-24 01:30:34 +0200

    Update App.jsx
    fix build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo } from 'react';
-import supabase from 'supabaseClient';
+import { supabase } from './supabaseClient';
 import PromptSidebar from './components/PromptSidebar';
 import PromptCard from './components/PromptCard';
 import PromptFormModal from './components/PromptFormModal';
