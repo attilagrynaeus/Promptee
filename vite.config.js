d2825922a438684ff4fe01632b6fdebe2193799e
unknown <2124090695+attilagrynaeus@users.noreply.github.com>
2025-05-24 21:03:09 +0200

    implementing lots of things  Z4-201-4

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: { // ⬅️ Ez a rész kerüljön ide az export blokkon belülre!
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
 });
